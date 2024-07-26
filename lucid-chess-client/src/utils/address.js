15fccbd55114549c80c6ffc77361a4f1d2960bbe
softdev0810 <laideming935@gmail.com>
2024-07-26 17:41:48 +1000

    version1

---
+++ lucid-chess-client/src/utils/address.js
@@ -0,0 +1,3 @@
+export const llgContractAddress = "0x0e04Ad5f9a40ddfe82ac533C9D5284Fb3C72BcAa";
+export const llgRewardContractAddress = "0xDc05A3751aF13b22c25F86Db686346e2c1648159";
+export const chainId = 0x61;
